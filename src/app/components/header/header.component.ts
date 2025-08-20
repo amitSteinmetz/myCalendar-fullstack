cc15dcb086f28938cde8b3fdf9074dccda7dc4ad
amitSteinmetz <117719977+amitSteinmetz@users.noreply.github.com>
2025-08-20 12:56:31 +0300

    finish header style expect click on menu design

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  readonly mainLinks = [
+    'לוח שנה עברי לועזי',
+    'לוח שנה שנתי',
+    'לוח חגים ומועדים',
+    'זמני היום והשבת',
+    'כניסת שבת וחג',
+    'מלונות בוקינג',
+  ];
+
+  readonly additionalLinks = [
+    'זמני היום לשנה',
+    'זמני זריחה ושקיעה',
+    'אודות לוח השנה',
+    'יצירת קשר',
+  ];
+
+  // isFirstLink(index: number) {
+  //   return index ==0
+  // }
+}
