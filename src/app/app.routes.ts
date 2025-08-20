590827052af6fddcafa8cf6ea2bff13a504562a3
amitSteinmetz <117719977+amitSteinmetz@users.noreply.github.com>
2025-08-20 17:00:35 +0300

    more of footer style and started time-navigation component styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,6 @@
 import { Routes } from '@angular/router';
+import { TimeNavigationComponent } from './components/time-navigation/time-navigation.component';
 
 export const routes: Routes = [
+  { path: '', component: TimeNavigationComponent },
 ];
