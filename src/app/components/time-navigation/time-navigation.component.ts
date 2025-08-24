b7e2849e7cb5dc3d7adc98ac3ea9ea816ddc8df3
amitSteinmetz <117719977+amitSteinmetz@users.noreply.github.com>
2025-08-24 10:39:12 +0300

    finish styling time-navigation component

--- src/app/components/time-navigation/time-navigation.component.ts
+++ src/app/components/time-navigation/time-navigation.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-time-navigation',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './time-navigation.component.html',
   styleUrl: './time-navigation.component.scss'
 })
 export class TimeNavigationComponent {
-
+  isEnglishMode: boolean = true;
 }
