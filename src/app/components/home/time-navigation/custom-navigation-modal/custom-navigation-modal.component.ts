a5f0aa823a84a239dc350bb702bfb40e687edcf5
amitSteinmetz <117719977+amitSteinmetz@users.noreply.github.com>
2025-08-26 23:11:02 +0300

    added few things to custom time nav comp

--- src/app/components/home/time-navigation/custom-navigation-modal/custom-navigation-modal.component.ts
+++ src/app/components/home/time-navigation/custom-navigation-modal/custom-navigation-modal.component.ts
@@ -1,12 +1,21 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  MONTHS_HEBREW,
+  MONTHS_GREGORIAN,
+  HOLIDAYS,
+} from '../../../../constants/dates';
+import { CommonModule } from '@angular/common';
 
 @Component({
-selector: 'app-custom-navigation-modal',
-  imports: [],
+  selector: 'app-custom-navigation-modal',
+  imports: [CommonModule],
   templateUrl: './custom-navigation-modal.component.html',
   styleUrl: './custom-navigation-modal.component.scss',
 })
 export class CustomNavigationModalComponent {
+  monthsHebrew = MONTHS_HEBREW;
+  monthsGregorian = MONTHS_GREGORIAN;
+  holidays = HOLIDAYS;
   @Output() close_modal: EventEmitter<void> = new EventEmitter();
 
   onCloseModalButtonClicked() {
--- src/app/components/home/time-navigation/time-navigation.component.ts
+++ src/app/components/home/time-navigation/time-navigation.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CustomNavigationModalComponent } from "./custom-navigation-modal/custom-navigation-modal.component";
+import { CustomNavigationModalComponent } from './custom-navigation-modal/custom-navigation-modal.component';
 
 @Component({
   selector: 'app-time-navigation',
   imports: [CommonModule, CustomNavigationModalComponent],
   templateUrl: './time-navigation.component.html',
-  styleUrl: './time-navigation.component.scss'
+  styleUrl: './time-navigation.component.scss',
 })
 export class TimeNavigationComponent {
   isEnglishMode: boolean = true;
